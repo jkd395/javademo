b0eb3d96c12df7b40531028257d1a85ebbfb3c43
jkd395 <jkd395@163.com>
2020-04-25 10:04:30 +0800

    update java file

--- src/main/java/com/example/javademo/JavademoApplication.java
+++ src/main/java/com/example/javademo/JavademoApplication.java
@@ -1,11 +1,11 @@
 package com.example.javademo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Value;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //@SpringBootApplication
